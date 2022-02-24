1e3e580f7bc1f9c287da62ae2c89d203db6385ae
virat <virat.meduri@gmail.com>
2022-02-24 17:07:29 +0530

    change function

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
 function createBoxes() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
       const box = document.createElement('div')
       box.classList.add('box')
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
